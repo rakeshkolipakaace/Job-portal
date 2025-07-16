e07eaf0aae722e11a374b73f918f34e27438b123
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-16 22:44:24 +0530

    models

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+import validator from "validator";
+
+const userSchema=new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:[true,'Name is required'],
+    },
+
+    lastName:{
+        typr:String,
+    },
+
+    email:{
+
+        type:String,
+        required:[true,'Email is required'],
+        unique:true,
+        validate:validator.isEmail,
+
+    },
+
+    password:{
+        type:String,
+        required:[true,'Password is required'],
+    },
+
+    location:{
+        type:String,
+        default:"My City"
+
+    }
+
+},{timestamps:true});
+
+
+export default mongoose.model("User", userSchema);
