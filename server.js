9285d48f45c23066d0a1a3eeefa2636de8d1e1db
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-16 23:11:53 +0530

    routing   server.js

--- server.js
+++ server.js
@@ -14,15 +14,14 @@ import cors from "cors";
 
 import morgan from "morgan";
 
+import authRoutes from "./routes/authRoutes.js";
 
 //config
 dotenv.config(); // dotenv.config(path:'path of env ') if the env is in another file
 
 connectDB();
 
-
-const app=express();
-
+const app = express();
 
 // app.get('/',(req,res)=>{
 //     res.send("<h1>Welcome to Job Portal</h1>")
@@ -33,16 +32,17 @@ const app=express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-
-
-app.use('/api/v1/test',testRoutes)
+//routes
+app.use("/api/v1/test", testRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 //port....
- const PORT=process.env.PORT || 4000;
-app.listen(PORT,()=>{
-
-    console.log(`Server is running In ${process.env.DEV_MODE} mode on port ${PORT}`.blue.bold);
-
-})
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(
+    `Server is running In ${process.env.DEV_MODE} mode on port ${PORT}`.blue
+      .bold
+  );
+});
