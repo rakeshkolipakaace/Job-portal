d916d712b022f531b9ed4440bbae002b4d33f7e2
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-17 07:40:16 +0530

    error handing by npm express-async-errors

--- middlewares/errorMiddelware.js
+++ middlewares/errorMiddelware.js
@@ -10,13 +10,32 @@
 // };
 
 // export default errorMiddleware;
-
 const errorMiddleware = (err, req, res, next) => {
-  console.error(err.stack); // Optional: logs the error stack in the console
+  console.error(err.stack);
 
-  res.status(500).json({
-    success: false,
+  const defaultErrors = {
+    statusCode: 500,
     message: err.message || "Internal Server Error",
+  };
+
+  //  Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    defaultErrors.statusCode = 400;
+    defaultErrors.message = message.join(", ");
+  }
+
+  //  Duplicate Key Error
+  if (err.code && err.code === 11000) {
+    const field = Object.keys(err.keyValue)[0];
+    defaultErrors.statusCode = 400;
+    defaultErrors.message = `${field} must be unique`;
+  }
+
+  // Send response at the end
+  res.status(defaultErrors.statusCode).json({
+    success: false,
+    message: defaultErrors.message,
   });
 };
 
