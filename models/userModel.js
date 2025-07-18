b85ccbb22594cc5019bb7f7b87c0fdd1dbb97b4e
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-18 07:22:54 +0530

    jwt token

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,8 @@ import validator from "validator";
 
 import bcrypt from "bcryptjs";
 
+import JWT from "jsonwebtoken";
+
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -25,6 +27,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, "Password is required"],
+      select: true,
     },
 
     location: {
@@ -41,5 +44,13 @@ userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+//jsonwebtoken
+
+userSchema.methods.createJWT = function () {
+  return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
 export default mongoose.model("User", userSchema);
